0811acee4bb39f7e814bf2dff5bd1a4def62c91b
UematsuMiku <uematsu@mma.cs.tsukuba.ac.jp>
2019-05-17 14:44:11 +0900

    5/17

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,4 +1,10 @@
 class LunchController < ApplicationController
     def go
+
+        eataries = ['松のや','あすかて食堂','te.to.te'
+        ,'トタンコットンカフェ','711で弁当']
+
+        @recommend = eataries.sample
+
     end
 end
